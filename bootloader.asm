27b5c8db71cf8a6112b05c5ecc163203ef99fb7c
Ben Green <benjaminangusgreen@gmail.com>
2018-06-04 10:37:38 -0700

    Update bootloader.asm

--- bootloader.asm
+++ bootloader.asm
@@ -1,3 +1,24 @@
+bits 16
+
+mov ax, 0x07C0
+mov ds, ax
+mov ax, 0x07E0      # 07E0h = (07C00h+200h)/10h, beginning of stack segment.
+mov ss, ax
+mov sp, 0x2000      # 8k of stack space.
+
+call clearscreen
+
+push 0x0000
+call movecursor
+add sp, 2
+
+push msg
+call print
+add sp, 2
+
+cli
+hlt
+
 clearscreen:
     push bp
     mov bp, sp
@@ -15,12 +36,13 @@ clearscreen:
     mov sp, bp
     pop bp
     ret
+
 movecursor:
     push bp
     mov bp, sp
     pusha
 
-    mov dx, [bp+4]      # get the argument from the stack. |bp| = 2, |arg| = 2
+    mov dx, [bp+4]  # get the argument from the stack. |bp| = 2, |arg| = 2
     mov ah, 0x02        # set cursor position
     mov bh, 0x00        # page 0 - doesn't matter, we're not using double-buffering
     int 0x10
@@ -29,3 +51,30 @@ movecursor:
     mov sp, bp
     pop bp
     ret
+
+print:
+    push bp
+    mov bp, sp
+    pusha
+    mov si, [bp+4]      # grab the pointer to the data
+    mov bh, 0x00        # page number, 0 again
+    mov bl, 0x00        # foreground color, irrelevant - in text mode
+    mov ah, 0x0E        # print character to TTY
+.char:
+    mov al, [si]        # get the current char from our pointer position
+    add si, 1           # keep incrementing si until we see a null char
+    or al, 0
+    je .return          # end if the string is done
+    int 0x10            # print the character if we're not done
+    jmp .char           # keep looping
+.return:
+    popa
+    mov sp, bp
+    pop bp
+    ret
+
+
+msg:    db "Oh boy do I sure love assembly!", 0
+
+times 510-($-$$) db 0
+dw 0xAA55
